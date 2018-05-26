1262d917ea752c4de9f7cee71a6801468eb50764
minwinmin <takuro.mikami@gmail.com>
2018-05-26 11:40:32 +0900

    Add files via upload

---
+++ 20180525_stepping_success_arduino.ino
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include "Stepper_28BYJ_48.h"
+
+// Circuit : https://github.com/thomasfredericks/Stepper_28BYJ_48/blob/master/examples/stepper_28BYJ_example/stepper_28BYJ_example.png
+
+
+Stepper_28BYJ_48 stepper(13,12,11,10);
+
+//////////////////////////////////////////////////////////////////////////////
+void setup() {
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
+void loop() {
+         stepper.step(-1);
+}
+
+
+
